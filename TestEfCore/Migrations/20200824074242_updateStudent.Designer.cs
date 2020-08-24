e393e27f24ebd51e4c9b007da8495710c1b26121
黄威龙 <962589950@qq.com>
2020-08-24 15:43:43 +0800

    1111

--- TestEfCore/Migrations/20200824074242_updateStudent.Designer.cs
+++ TestEfCore/Migrations/20200824074242_updateStudent.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestEfCore;
 
 namespace TestEfCore.Migrations
 {
     [DbContext(typeof(TestEfDbContext))]
-    partial class TestEfDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200824074242_updateStudent")]
+    partial class updateStudent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
